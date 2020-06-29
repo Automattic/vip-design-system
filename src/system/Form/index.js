47b10cad115d20620bb23298482d88a43598255b
Saxon Fletcher <saxonafletcher@gmail.com>
2020-06-30 08:56:23 +1000

    fix linting issues

--- src/system/Form/index.js
+++ src/system/Form/index.js
@@ -1,13 +1,13 @@
 /**
  * Internal dependencies
  */
-import { Input } from "./Input";
-import { Label } from "./Label";
-import { Toggle } from "./Toggle";
-import { Validation } from "./Validation";
-import { ToggleRow } from "./ToggleRow";
-import { Select } from "./Select";
-import { Radio } from "./Radio";
-import { Textarea } from "./Textarea";
+import { Input } from './Input';
+import { Label } from './Label';
+import { Toggle } from './Toggle';
+import { Validation } from './Validation';
+import { ToggleRow } from './ToggleRow';
+import { Select } from './Select';
+import { Radio } from './Radio';
+import { Textarea } from './Textarea';
 
 export { Input, Label, Radio, Select, Textarea, Toggle, ToggleRow, Validation };
--- src/system/index.js
+++ src/system/index.js
@@ -1,48 +1,30 @@
 /**
  * Internal dependencies
  */
-import { Avatar } from "./Avatar";
-import { Badge } from "./Badge";
-import { BlankState } from "./BlankState";
-import { Box } from "./Box";
-import { Button } from "./Button";
-import { Card } from "./Card";
-import {
-	Dialog,
-	DialogDivider,
-	DialogMenu,
-	DialogMenuItem,
-	DialogContent,
-} from "./Dialog";
-import {
-	ConfirmationDialog,
-	ConfirmationDialogContent,
-} from "./ConfirmationDialog";
-import { Flex } from "./Flex";
-import {
-	Input,
-	Label,
-	Toggle,
-	ToggleRow,
-	Validation,
-	Radio,
-	Select,
-	Textarea,
-} from "./Form";
-import { Grid } from "./Grid";
-import { Heading } from "./Heading";
-import { Link } from "./Link";
-import { Notice } from "./Notice";
-import { Progress } from "./Progress";
-import { Spinner } from "./Spinner";
-import { UsageChart } from "./UsageChart";
-import { Notification } from "./Notification";
-import { OptionRow } from "./OptionRow";
-import { Table, TableRow } from "./Table";
-import { TabItem, Tabs } from "./Tabs";
-import { Text } from "./Text";
-import theme from "./theme";
-import { Wizard, WizardStep, WizardStepHorizontal } from "./Wizard";
+import { Avatar } from './Avatar';
+import { Badge } from './Badge';
+import { BlankState } from './BlankState';
+import { Box } from './Box';
+import { Button } from './Button';
+import { Card } from './Card';
+import { Dialog, DialogDivider, DialogMenu, DialogMenuItem, DialogContent } from './Dialog';
+import { ConfirmationDialog, ConfirmationDialogContent } from './ConfirmationDialog';
+import { Flex } from './Flex';
+import { Input, Label, Toggle, ToggleRow, Validation, Radio, Select, Textarea } from './Form';
+import { Grid } from './Grid';
+import { Heading } from './Heading';
+import { Link } from './Link';
+import { Notice } from './Notice';
+import { Progress } from './Progress';
+import { Spinner } from './Spinner';
+import { UsageChart } from './UsageChart';
+import { Notification } from './Notification';
+import { OptionRow } from './OptionRow';
+import { Table, TableRow } from './Table';
+import { TabItem, Tabs } from './Tabs';
+import { Text } from './Text';
+import theme from './theme';
+import { Wizard, WizardStep, WizardStepHorizontal } from './Wizard';
 
 export {
 	Avatar,
