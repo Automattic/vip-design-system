30cd244ff23797d207539a7d76e29a7ba331cc9b
Saxon Fletcher <saxonafletcher@gmail.com>
2020-07-14 11:27:19 +1000

    update avatar

--- src/system/Avatar/Avatar.js
+++ src/system/Avatar/Avatar.js
@@ -5,24 +5,55 @@
 import { jsx } from 'theme-ui';
 import PropTypes from 'prop-types';
 
-const Avatar = ( { size = 32, src = null, ...props } ) => (
-	<img
-		src={src}
-		sx={{
+/**
+ * Internal dependencies
+ */
+import { Box, Text } from 'system';
+
+const Avatar = ( { size = 32, src = null, name = null, ...props } ) => (
+	<Box
+		sx={ {
 			borderRadius: 9999,
 			height: size,
 			width: size,
 			border: '2px solid',
 			borderColor: 'primary',
-			display: 'block',
-		}}
-		{...props}
-	/>
+			overflow: 'hidden',
+			backgroundColor: 'brand.50',
+			color: 'white',
+			textAlign: 'center',
+		} }
+		{ ...props }
+	>
+		{ src ? (
+			<img
+				src={ src }
+				alt={ name }
+				sx={ {
+					width: '100%',
+					display: 'block',
+				} }
+			/>
+		) : (
+			<Text
+				as="span"
+				sx={ {
+					color: 'white',
+					fontWeight: 'bold',
+					fontSize: 0,
+					textTransform: 'uppercase',
+				} }
+			>
+				{ name ? name.charAt( 0 ) : null }
+			</Text>
+		) }
+	</Box>
 );
 
 Avatar.propTypes = {
 	size: PropTypes.number,
 	src: PropTypes.string,
+	name: PropTypes.string,
 };
 
 export { Avatar };
