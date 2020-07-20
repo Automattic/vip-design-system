e0d81362dc6f79473ee34f94bcc773c5fb1d1588
Saxon Fletcher <saxonafletcher@gmail.com>
2020-07-20 14:01:30 +1000

    code component

---
+++ src/system/Code/Code.stories.js
@@ -0,0 +1,18 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+import { action } from '@storybook/addon-actions';
+
+/**
+ * Internal dependencies
+ */
+import { Code } from '..';
+
+export default {
+	title: 'Code',
+	component: Code,
+};
+
+export const Default = () => <Code>Code</Code>;
