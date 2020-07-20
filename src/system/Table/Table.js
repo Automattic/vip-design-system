44bfe67081ee1dd75e2611e91a94721e220244e3
Saxon Fletcher <saxonafletcher@gmail.com>
2020-07-20 17:37:08 +1000

    adding padding to table to handle dropdown

--- src/system/Table/Table.js
+++ src/system/Table/Table.js
@@ -5,7 +5,7 @@
 import { jsx } from 'theme-ui';
 
 const Table = props => (
-	<div sx={ { overflowX: 'auto' } }>
+	<div sx={ { overflowX: 'auto', pb: 300, mb: -300 } }>
 		<table
 			sx={ { width: '100%', minWidth: 1024 } }
 			cellPadding={ 0 }
