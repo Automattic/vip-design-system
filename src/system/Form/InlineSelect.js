5ff027635f4f0e6a5b2fdfcfe6126558d22ba8e6
Saxon Fletcher <saxonafletcher@gmail.com>
2021-07-19 09:49:20 +1000

    code formatting

--- src/system/Form/InlineSelect.js
+++ src/system/Form/InlineSelect.js
@@ -5,20 +5,32 @@
 import { jsx } from 'theme-ui';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { MdDone } from 'react-icons/md';
-import Select, { components } from 'react-select';
+import Select from 'react-select';
 
 /**
  * Internal dependencies
  */
-import { theme as vipTheme, Box, Dialog, DialogButton, Text } from '..';
+import { theme as vipTheme, Box, Dialog, DialogButton } from '..';
 import { Option } from './SearchSelect';
 
 const selectStyles = {
-	control: provided => ( { ...provided, minWidth: 240, margin: 0, border: 0, padding: 0, boxShadow: 'none', fontSize: vipTheme.fontSizes[ 2 ] } ),
+	control: provided => ( {
+		...provided,
+		minWidth: 240,
+		margin: 0,
+		border: 0,
+		padding: 0,
+		boxShadow: 'none',
+		fontSize: vipTheme.fontSizes[ 2 ],
+	} ),
 	container: provided => ( { ...provided, padding: 0 } ),
 	input: provided => ( { ...provided, padding: 0 } ),
-	option: provided => ( { ...provided, paddingTop: vipTheme.space[ 1 ], paddingBottom: vipTheme.space[ 1 ], paddingLeft: vipTheme.space[ 2 ] } ),
+	option: provided => ( {
+		...provided,
+		paddingTop: vipTheme.space[ 1 ],
+		paddingBottom: vipTheme.space[ 1 ],
+		paddingLeft: vipTheme.space[ 2 ],
+	} ),
 	menu: () => ( { boxShadow: 'none', borderTop: `1px solid ${ vipTheme.colors.border }` } ),
 };
 
@@ -37,19 +49,19 @@ const InlineSelect = ( { label, value, options, noneLabel = 'All', ...props } ) 
 		<Box>
 			<Select
 				autoFocus
-				backspaceRemovesValue={ false }
-				components={ { Option, DropdownIndicator: null, IndicatorSeparator: null } }
-				controlShouldRenderValue={ false }
-				hideSelectedOptions={ false }
-				isClearable={ false }
+				backspaceRemovesValue={false}
+				components={{ Option, DropdownIndicator: null, IndicatorSeparator: null }}
+				controlShouldRenderValue={false}
+				hideSelectedOptions={false}
+				isClearable={false}
 				menuIsOpen
-				options={ options }
-				styles={ selectStyles }
+				options={options}
+				styles={selectStyles}
 				placeholder="Search..."
-				tabSelectsValue={ false }
-				value={ value }
+				tabSelectsValue={false}
+				value={value}
 				{...props}
-				theme={ theme => ( {
+				theme={theme => ( {
 					...theme,
 					colors: {
 						...theme.colors,
@@ -57,7 +69,7 @@ const InlineSelect = ( { label, value, options, noneLabel = 'All', ...props } ) 
 						primary50: vipTheme.colors.brand[ '7' ],
 						primary: vipTheme.colors.heading,
 					},
-				} ) }
+				} )}
 			/>
 		</Box>
 	);
@@ -65,28 +77,7 @@ const InlineSelect = ( { label, value, options, noneLabel = 'All', ...props } ) 
 	return (
 		<Dialog
 			content={Content}
-			trigger={
-				<DialogButton sx={ { width: '100%' } }>
-					{ label &&
-						<Text as="span" sx={{ mb: 0, color: 'muted', mr: 2, flex: '0 0 auto' }}>
-							{label}:
-						</Text>
-					}
-					<Text
-						as="span"
-						sx={ {
-							mb: 0,
-							flex: '1 1 auto',
-							'white-space': 'nowrap',
-							overflow: 'hidden',
-							color: 'heading',
-							'text-overflow': 'ellipsis',
-						} }
-					>
-						{ valueLabel }
-					</Text>
-				</DialogButton>
-			}
+			trigger={<DialogButton sx={{ width: '100%' }} label={label} value={valueLabel} />}
 		/>
 	);
 };
--- src/system/Form/SearchSelect.js
+++ src/system/Form/SearchSelect.js
@@ -13,23 +13,45 @@ import PropTypes from 'prop-types';
 import { theme as vipTheme, Flex, Text } from '..';
 
 export const selectStyles = {
-	control: provided => ( { ...provided, border: `1px solid ${ vipTheme.colors.border }`, margin: 0, padding: 0, boxShadow: 'none', fontSize: vipTheme.fontSizes[ 2 ] } ),
-	option: provided => ( { ...provided, paddingTop: vipTheme.space[ 1 ], paddingBottom: vipTheme.space[ 1 ], paddingLeft: vipTheme.space[ 2 ] } ),
+	control: provided => ( {
+		...provided,
+		border: `1px solid ${ vipTheme.colors.border }`,
+		margin: 0,
+		padding: 0,
+		boxShadow: 'none',
+		fontSize: vipTheme.fontSizes[ 2 ],
+	} ),
+	option: provided => ( {
+		...provided,
+		paddingTop: vipTheme.space[ 1 ],
+		paddingBottom: vipTheme.space[ 1 ],
+		paddingLeft: vipTheme.space[ 2 ],
+	} ),
 	menu: provided => ( { ...provided, boxShadow: vipTheme.shadows.low } ),
 	indicatorSeparator: () => ( { display: 'none' } ),
-	valueContainer: provided => ( { ...provided, paddingLeft: vipTheme.space[ 1 ], paddingRight: vipTheme.space[ 1 ] } ),
-	singleValue: provided => ( { ...provided, paddingLeft: vipTheme.space[ 1 ], paddingRight: vipTheme.space[ 1 ] } ),
+	valueContainer: provided => ( {
+		...provided,
+		paddingLeft: vipTheme.space[ 1 ],
+		paddingRight: vipTheme.space[ 1 ],
+	} ),
+	singleValue: provided => ( {
+		...provided,
+		paddingLeft: vipTheme.space[ 1 ],
+		paddingRight: vipTheme.space[ 1 ],
+	} ),
 };
 
 export const Option = ( { data, isSelected, ...props } ) => (
-	<components.Option { ...props }>
-		<Flex sx={ { alignItems: 'center' } }>
-			{ isSelected &&
-				<Text as="span" sx={ { mb: 0, mr: 2, color: 'green.60', svg: { display: 'block' } } }>
+	<components.Option {...props}>
+		<Flex sx={{ alignItems: 'center' }}>
+			{isSelected && (
+				<Text as="span" sx={{ mb: 0, mr: 2, color: 'green.60', svg: { display: 'block' } }}>
 					<MdDone />
 				</Text>
-			}
-			<Text as="span" sx={ { mb: 0, flex: '1 1 auto', color: 'heading' } }>{ data.label }</Text>
+			)}
+			<Text as="span" sx={{ mb: 0, flex: '1 1 auto', color: 'heading' }}>
+				{data.label}
+			</Text>
 		</Flex>
 	</components.Option>
 );
@@ -39,8 +61,10 @@ Option.propTypes = {
 	isSelected: PropTypes.bool,
 };
 
-export const DropdownIndicator = props => <MdExpandMore { ...props } sx={ { color: 'text', mr: 2 } } />;
-const ClearIndicator = ( { innerProps: { ref, ...restInnerProps }, ...props } ) => <MdClose ref={ ref } { ...restInnerProps } { ...props } sx={ { color: 'text', mr: 2 } } />;
+export const DropdownIndicator = props => <MdExpandMore {...props} sx={{ color: 'text', mr: 2 }} />;
+const ClearIndicator = ( { innerProps: { ref, ...restInnerProps }, ...props } ) => (
+	<MdClose ref={ref} {...restInnerProps} {...props} sx={{ color: 'text', mr: 2 }} />
+);
 
 ClearIndicator.propTypes = {
 	innerProps: PropTypes.object,
@@ -49,9 +73,9 @@ ClearIndicator.propTypes = {
 const SearchSelect = props => (
 	<Select
 		{...props}
-		components={ { Option, DropdownIndicator, ClearIndicator } }
+		components={{ Option, DropdownIndicator, ClearIndicator }}
 		styles={selectStyles}
-		theme={ theme => ( {
+		theme={theme => ( {
 			...theme,
 			colors: {
 				...theme.colors,
@@ -61,7 +85,7 @@ const SearchSelect = props => (
 				dangerLight: vipTheme.colors.red[ '10' ],
 				danger: vipTheme.colors.red[ '70' ],
 			},
-		} ) }
+		} )}
 	/>
 );
 
