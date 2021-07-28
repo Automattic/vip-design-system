c883a850ff8c2d825871804599581f69bedf8347
Bruno Basto <bruno.basto@a8c.com>
2021-07-28 10:10:59 -0300

    Adds build files

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	"ignore": [
+		"**/*.stories.js"
+	],
+	"presets": [
+		[
+			"@babel/preset-env",
+			{
+				"loose": true
+			}
+		],
+		[
+			"@babel/preset-react",
+			{
+				"importSource": "theme-ui",
+				"runtime": "automatic",
+				"throwIfNamespace": false,
+			},
+		],
+	],
+};
