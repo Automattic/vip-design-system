200c4df4bb6e41e508c064763e0ae4dcc1c101d7
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2021-08-25 10:06:32 -0300

    Improves Notice component to handle alignment and dark mode

--- src/system/Notice/Notice.stories.js
+++ src/system/Notice/Notice.stories.js
@@ -5,6 +5,7 @@
 /**
  * Internal dependencies
  */
+import React from 'react';
 import { Heading, Notice, Text } from '..';
 
 export default {
@@ -13,10 +14,16 @@ export default {
 };
 
 export const Default = () => (
-	<Notice variant="alert" sx={{ mb: 4 }}>
-		<Heading variant="h4">Your site is ready to launch!</Heading>
-		<Text sx={{ mb: 0 }}>
-			It looks like you&lsquo;re ready to share your application with the world.
-		</Text>
-	</Notice>
+	<React.Fragment>
+		<Notice variant="alert" sx={{ mb: 4 }}>
+			<Heading variant="h4">Your site is ready to launch!</Heading>
+			<Text sx={{ mb: 0 }}>
+				It looks like you&lsquo;re ready to share your application with the world.
+			</Text>
+		</Notice>
+
+		<Notice variant="success" sx={{ mb: 4 }}>
+			It looks like you&lsquo;re ready to share your <a href="#">application with the world.</a>
+		</Notice>
+	</React.Fragment>
 );
