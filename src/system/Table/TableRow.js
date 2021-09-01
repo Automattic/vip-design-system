19d6d11d2f97a7da57f49b9ab284d5fb742561f0
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2021-08-31 23:55:27 -0300

    Moving from vip-ui new TableRow

--- src/system/Table/TableRow.js
+++ src/system/Table/TableRow.js
@@ -1,16 +1,20 @@
+/** @jsxImportSource theme-ui */
+
+/**
+ * External dependencies
+ */
+
 import PropTypes from 'prop-types';
 
 /**
  * Internal dependencies
  */
-import { Heading } from '../';
+import { Heading, Box } from '../';
 
-const TableRow = ( { onClick, head = false, cells } ) => {
+const TableRow = ( { onClick, head = false, rowHead = false, cells } ) => {
 	const hoverStyles = onClick
 		? { cursor: 'pointer', '&:hover': { bg: 'hover', borderRadius: 2 } }
 		: {};
-
-	const CellElement = head ? 'th' : 'td';
 
 	const handleKeyPress = e => {
 		if ( e.key === 'Enter' && onClick ) {
@@ -26,37 +30,58 @@ const TableRow = ( { onClick, head = false, cells } ) => {
 			onKeyDown={ handleKeyPress }
 		>
 			{ cells.map( ( cell, index ) => (
-				<CellElement
-					key={ index }
-					sx={ {
-						px: 3,
-						py: 2,
-						textAlign: 'left',
-						borderBottom: '1px solid',
-						borderTop: head ? '1px solid' : 'none',
-						borderColor: 'border',
-						'&:first-child': {
-							pl: 5,
-						},
-					} }
-				>
-					{ head ? (
-						<Heading variant="caps" as="div" sx={ { mb: 0 } }>
-							{ cell }
-						</Heading>
-					) : (
-						cell
-					) }
-				</CellElement>
+				<TableCell key={ index } cell={ cell } head={ head } isRowHead={ index === 0 && rowHead } />
 			) ) }
 		</tr>
 	);
+};
+
+const TableCell = ( { head, isRowHead, cell } ) => {
+	let scope = null;
+	if ( head ) {
+		scope = 'col';
+	} else if ( isRowHead ) {
+		scope = 'row';
+	}
+	return (
+		<Box
+			as={ isRowHead || head ? 'th' : 'td' }
+			scope={ scope }
+			sx={ {
+				px: 3,
+				py: 2,
+				textAlign: 'left',
+				borderBottom: '1px solid',
+				fontWeight: 'body',
+				borderTop: head ? '1px solid' : 'none',
+				borderColor: 'border',
+				'&:first-child': {
+					pl: 5,
+				},
+			} }
+		>
+			{ head ? (
+				<Heading variant="caps" as="div" sx={ { mb: 0 } }>
+					{ cell }
+				</Heading>
+			) : (
+				cell
+			) }
+		</Box>
+	);
+};
+
+TableCell.propTypes = {
+	head: PropTypes.bool,
+	isRowHead: PropTypes.bool,
+	cell: PropTypes.array,
 };
 
 TableRow.propTypes = {
 	onClick: PropTypes.func,
 	head: PropTypes.bool,
 	cells: PropTypes.array,
+	rowHead: PropTypes.bool,
 };
 
 export { TableRow };
