fc50f0c0b89285179dd2e218988ef200c6f97296
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-06-29 17:48:53 -0300

    Remove focus on elements

--- src/system/Notification/Notification.js
+++ src/system/Notification/Notification.js
@@ -11,55 +11,45 @@ import PropTypes from 'prop-types';
  */
 import { Box, Card, Flex, Text, Button } from '../';
 import ScreenReaderText from '../ScreenReaderText/ScreenReaderText';
-import { useLayoutEffect, useRef } from 'react';
 
-const Notification = ( { title, body, status = 'success', onClose } ) => {
-	const buttonRef = useRef();
+const Notification = ( { title, body, status = 'success', onClose } ) =>(
+	<Card
+		role="alert"
+		className="vip-notification-component"
+		sx={{
+			boxShadow: 'medium',
+			width: 320,
+			position: 'relative',
+			variant: `notification.${ status }`,
+		}}
+	>
+		<ScreenReaderText>Alert,</ScreenReaderText>
+		<Flex sx={{ alignItems: 'center' }}>
+			{status === 'error' ? (
+				<MdError sx={{ color: 'error', flex: '0 0 auto' }} aria-hidden="true" />
+			) : (
+				<MdCheckCircle sx={{ color: 'success', flex: '0 0 auto' }} aria-hidden="true" />
+			)}
+			<Box sx={{ flex: '1 1 auto', ml: 3 }}>
+				<p sx={{ my: 0, color: 'heading', fontWeight: 'bold' }}>
+					{title}
+				</p>
+				{body && <Text sx={{ mb: 0, mt: 1 }}>{body}</Text>}
+			</Box>
+		</Flex>
 
-	useLayoutEffect( () => {
-		buttonRef.current.focus();
-	}, [] );
-
-	return (
-		<Card
-			ref={buttonRef}
-			role="alert"
-			className="vip-notification-component"
-			sx={{
-				boxShadow: 'medium',
-				width: 320,
-				position: 'relative',
-				variant: `notification.${ status }`,
-			}}
-		>
-			<ScreenReaderText>Alert,</ScreenReaderText>
-			<Flex sx={{ alignItems: 'center' }}>
-				{status === 'error' ? (
-					<MdError sx={{ color: 'error', flex: '0 0 auto' }} aria-hidden="true" />
-				) : (
-					<MdCheckCircle sx={{ color: 'success', flex: '0 0 auto' }} aria-hidden="true" />
-				)}
-				<Box sx={{ flex: '1 1 auto', ml: 3 }}>
-					<p sx={{ my: 0, color: 'heading', fontWeight: 'bold' }}>
-						{title}
-					</p>
-					{body && <Text sx={{ mb: 0, mt: 1 }}>{body}</Text>}
-				</Box>
-			</Flex>
-
-			{ onClose && (
-				<Button
-					onClick={onClose}
-					variant="icon"
-					sx={{ color: 'muted', position: 'absolute', top: 2, right: 2 }}
-					aria-hidden="true"
-				>
-					<MdClose />
-				</Button>
-			) }
-		</Card>
-	);
-};
+		{ onClose && (
+			<Button
+				onClick={onClose}
+				variant="icon"
+				sx={{ color: 'muted', position: 'absolute', top: 2, right: 2 }}
+				aria-hidden="true"
+			>
+				<MdClose />
+			</Button>
+		) }
+	</Card>
+);
 
 Notification.propTypes = {
 	title: PropTypes.node,
