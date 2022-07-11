5531aeb904d6bf3ae19e6e73a40bb0aa890a1778
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-07-11 16:01:17 -0300

    Add DialogOverlay component

---
+++ src/system/NewDialog/DialogOverlay.js
@@ -0,0 +1,35 @@
+/** @jsxImportSource theme-ui */
+
+/**
+ * Internal dependencies
+ */
+import * as Dialog from '@radix-ui/react-dialog';
+
+export const DialogOverlay = props => (
+	<Dialog.Overlay
+		sx={ {
+			position: 'fixed',
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: 0,
+			inset: 0,
+			opacity: 0.7,
+			background: `linear-gradient(
+        198.09deg,
+        #E5F0F6 2.01%,
+        rgba(235, 238, 242, 0) 43.18%,
+        rgba(249, 234, 232, 0) 47.86%,
+        #FFE9D1 94.31%
+      ),
+      linear-gradient(
+        98.65deg,
+        #FFE8E6 0.58%,
+        rgba(255, 233, 214, 0) 52.45%,
+        rgba(255, 233, 219, 0) 53.76%,
+        #FFE9D1 105.86%
+      ), #F5F2F1`,
+		} }
+		{ ...props }
+	/>
+);
---
+++ src/system/NewDialog/DialogOverlay.test.js
@@ -0,0 +1,27 @@
+/**
+ * External dependencies
+ */
+import { Dialog } from '@radix-ui/react-dialog';
+import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+/**
+ * Internal dependencies
+ */
+import { DialogOverlay } from './DialogOverlay';
+
+// If you render any Dialog child withou the `<Dialog />` parent, it will throw an error.
+const Wrapper = props => <Dialog open={ true } { ...props } />;
+
+describe( '<DialogOverlay />', () => {
+	it( 'renders the DialogOverlay component', async () => {
+		const { container } = render(
+			<Wrapper>
+				<DialogOverlay />
+			</Wrapper>
+		);
+
+		// Check for accessibility issues
+		await expect( await axe( container ) ).toHaveNoViolations();
+	} );
+} );
