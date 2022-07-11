cb1947fda9a226eda85f56c3f13c4c91d7e63aa8
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-07-11 16:02:06 -0300

    Make stories with a nicer look

--- .storybook/decorators/withBoundingBox.jsx
+++ .storybook/decorators/withBoundingBox.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { makeDecorator } from '@storybook/addons';
-import { Box } from "../../src/system";
+import { Box, Heading } from '../../src/system';
 
 export default makeDecorator( {
 	name: 'withBoundingBox',
 	parameterName: 'colorMode2',
 	wrapper: ( storyFn, context ) => {
 		return (
-			<Box sx={{ p: 4, height: "100vh" }}>
-				{storyFn()}
+			<Box sx={ { p: 3, height: '100vh' } }>
+				<Box sx={ { backgroundColor: 'backgroundMuted', p: 3, borderRadius: 2 } }>
+					<Heading variant="h1" sx={ { mb: 1 } }>
+						{ context.title }
+					</Heading>
+					<Heading variant="h2">{ context.name }</Heading>
+				</Box>
+
+				<Box sx={ { py: 3 } }>{ storyFn() }</Box>
 			</Box>
 		);
-	}
+	},
 } );
