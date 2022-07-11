dffd500b0572c26afb863d739eed7a5668ed5068
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-07-11 16:38:23 -0300

    Export NewDialog and fix gradient

--- src/system/NewDialog/DialogOverlay.js
+++ src/system/NewDialog/DialogOverlay.js
@@ -15,20 +15,9 @@ export const DialogOverlay = props => (
 			bottom: 0,
 			inset: 0,
 			opacity: 0.7,
-			background: `linear-gradient(
-        198.09deg,
-        #E5F0F6 2.01%,
-        rgba(235, 238, 242, 0) 43.18%,
-        rgba(249, 234, 232, 0) 47.86%,
-        #FFE9D1 94.31%
-      ),
-      linear-gradient(
-        98.65deg,
-        #FFE8E6 0.58%,
-        rgba(255, 233, 214, 0) 52.45%,
-        rgba(255, 233, 219, 0) 53.76%,
-        #FFE9D1 105.86%
-      ), #F5F2F1`,
+			background:
+				// eslint-disable-next-line max-len
+				'linear-gradient(198.09deg,#E5F0F6 2.01%, rgba(235, 238, 242, 0) 43.18%,rgba(249, 234, 232, 0) 47.86%, #FFE9D1 94.31%), linear-gradient(98.65deg, #FFE8E6 0.58%, rgba(255, 233, 214, 0) 52.45%, rgba(255, 233, 219, 0) 53.76%,#FFE9D1 105.86%), #F5F2F1',
 		} }
 		{ ...props }
 	/>
