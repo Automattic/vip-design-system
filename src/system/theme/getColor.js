d196682b0c30fadf62ecb49991711414731ff693
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-07-20 20:40:11 -0300

    Use token-transformer tool to generate theme for the VIP Dashboard

--- src/system/theme/getColor.js
+++ src/system/theme/getColor.js
@@ -2,28 +2,12 @@
  * Internal dependencies
  */
 
-import Valet from '../../../tokens/valet-color.json';
-
-const ThemeColors = Valet[ 'productive-color-wpvip' ];
-const CoreColors = Valet[ 'valet-core' ];
+import Valet from '../../generated/valet-theme.json';
 
 export const getColor = ( color, variant = 'default' ) => {
-	if ( ! ThemeColors[ color ] ) {
+	if ( ! Valet[ color ] ) {
 		return '#000000';
 	}
 
-	const colorValue = ThemeColors[ color ][ variant ].value;
-
-	if ( colorValue.startsWith( '{' ) ) {
-		const [ name, number ] = colorValue.replace( /[{}]/g, '' ).split( '.' );
-
-		// Some colors are flat, there is no sub values.
-		if ( CoreColors[ name ][ number ] ) {
-			return CoreColors[ name ][ number ].value;
-		}
-
-		return CoreColors[ name ].value;
-	}
-
-	return colorValue;
+	return Valet[ color ][ variant ].value;
 };
