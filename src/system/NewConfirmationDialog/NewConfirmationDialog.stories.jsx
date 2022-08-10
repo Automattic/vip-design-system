8d3776b0acaa2ff453c650fae5cb908740dc2582
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-08-10 10:48:52 -0300

    Add new NewConfirmationDialog component

---
+++ src/system/NewConfirmationDialog/NewConfirmationDialog.stories.jsx
@@ -0,0 +1,32 @@
+/**
+ * Internal dependencies
+ */
+import React from 'react';
+import { Box, NewConfirmationDialog, Button } from '..';
+
+export default {
+	title: 'NewConfirmationDialog',
+	component: NewConfirmationDialog,
+};
+
+const ConfirmationTrigger = <Button sx={ { mr: 3 } }>Click to answer</Button>;
+
+export const Default = () => {
+	const [ answer, setAnswer ] = React.useState( '🤔' );
+	return (
+		<Box>
+			<p>Confirm that your name is John doe?</p>
+			<NewConfirmationDialog
+				title={ 'Are you John Doe?' }
+				description={ 'Please confirm that your name is John Doe.' }
+				trigger={ ConfirmationTrigger }
+				body="A modal is used to perform more detailed actions that don&lsquo;t necessarily need the context
+					behind."
+				onConfirm={ () => setAnswer( '👍' ) }
+				needsConfirm={ true }
+			/>
+
+			<p>Answer: { answer }</p>
+		</Box>
+	);
+};
