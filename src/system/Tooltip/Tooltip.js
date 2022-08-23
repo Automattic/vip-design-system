7fb2e776c44e5bff9200ebeb3dd778ece813d0a4
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-08-23 12:10:25 -0300

    Update Radix Primitives dependencies to 1.0.0

--- src/system/Tooltip/Tooltip.js
+++ src/system/Tooltip/Tooltip.js
@@ -5,14 +5,14 @@
  */
 import PropTypes from 'prop-types';
 import { MdHelp } from 'react-icons/md';
-import * as TTip from '@radix-ui/react-tooltip';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 
 /**
  * Internal dependencies
  */
 import { Card, Text } from '..';
 
-const StyledArrow = props => <TTip.Arrow sx={ { fill: 'white' } } { ...props } />;
+const StyledArrow = props => <TooltipPrimitive.Arrow sx={ { fill: 'white' } } { ...props } />;
 
 const Tooltip = ( {
 	trigger = <MdHelp />,
@@ -23,45 +23,47 @@ const Tooltip = ( {
 	...props
 } ) => {
 	return (
-		<TTip.Root
-			skipDelayDuration={ 700 }
-			sx={ {
-				display: 'inline-block',
-				position: 'relative',
-				svg: {
-					display: 'block',
-				},
-				'.tooltip-content': {
-					visibility: 'hidden',
-				},
-				'&:hover': {
-					'.tooltip-content': {
-						visibility: 'visible',
-					},
-				},
-			} }
-			{ ...tooltipProps }
-		>
-			<TTip.Trigger
+		<TooltipPrimitive.Provider>
+			<TooltipPrimitive.Root
+				skipDelayDuration={ 700 }
 				sx={ {
-					background: 'transparent',
-					border: 'none',
-					display: 'inline-flex',
-					outline: 'none',
-					p: 0,
-					m: 0,
+					display: 'inline-block',
+					position: 'relative',
+					svg: {
+						display: 'block',
+					},
+					'.tooltip-content': {
+						visibility: 'hidden',
+					},
+					'&:hover': {
+						'.tooltip-content': {
+							visibility: 'visible',
+						},
+					},
 				} }
+				{ ...tooltipProps }
 			>
-				{ trigger }
+				<TooltipPrimitive.Trigger
+					sx={ {
+						background: 'transparent',
+						border: 'none',
+						display: 'inline-flex',
+						outline: 'none',
+						p: 0,
+						m: 0,
+					} }
+				>
+					{ trigger }
 
-				<TTip.Content>
-					<Card className="tooltip-content" sx={ { width: width } } { ...props }>
-						{ children ? children : <Text sx={ { fontSize: 1 } }>{ text }</Text> }
-					</Card>
-					<StyledArrow />
-				</TTip.Content>
-			</TTip.Trigger>
-		</TTip.Root>
+					<TooltipPrimitive.Content>
+						<Card className="tooltip-content" sx={ { width: width } } { ...props }>
+							{ children ? children : <Text sx={ { fontSize: 1 } }>{ text }</Text> }
+						</Card>
+						<StyledArrow />
+					</TooltipPrimitive.Content>
+				</TooltipPrimitive.Trigger>
+			</TooltipPrimitive.Root>
+		</TooltipPrimitive.Provider>
 	);
 };
 
