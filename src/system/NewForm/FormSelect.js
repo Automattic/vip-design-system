65452b558f6d8d9184b0e10230bf35878e6b9671
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-09-08 18:17:39 -0300

    Extract FormSelectContent

--- src/system/NewForm/FormSelect.js
+++ src/system/NewForm/FormSelect.js
@@ -5,12 +5,13 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MdExpandMore } from 'react-icons/md';
 import { Label } from '../Form/Label';
 
 /**
  * Internal dependencies
  */
+import { FormSelectArrow } from './FormSelectArrow';
+import { FormSelectContent } from './FormSelectContent';
 
 const MAX_SUGGESTED_OPTIONS = 15;
 const isDev = process.env.NODE_ENV !== 'production';
@@ -31,45 +32,6 @@ const renderGroup = ( groupLabel, groupOptions ) => {
 	);
 };
 
-const defaultStyles = {
-	'&:hover select': { borderColor: 'border' },
-	display: 'inline-flex',
-	flexDirection: 'row',
-	alignItems: 'center',
-};
-
-const inlineStyles = {
-	position: 'relative',
-	display: 'inline-flex',
-	alignItems: 'center',
-	borderColor: 'border',
-	borderRadius: 1,
-	borderWidth: 1,
-	borderStyle: 'solid',
-	paddingRight: 0,
-	paddingLeft: 3,
-
-	label: {
-		margin: 0,
-		paddingRight: 2,
-		borderRightWidth: '1px',
-		borderRightStyle: 'solid',
-		borderRightColor: 'border',
-	},
-
-	select: {
-		width: '100%',
-		border: 'none',
-		margin: 0,
-		paddingLeft: 2,
-	},
-
-	svg: {
-		right: 2,
-		position: 'absolute',
-	},
-};
-
 const FormSelect = React.forwardRef(
 	( { isInline, placeholder, forLabel, options, label, ...props }, forwardRef ) => {
 		if ( isDev && options.length > MAX_SUGGESTED_OPTIONS ) {
@@ -80,55 +42,38 @@ const FormSelect = React.forwardRef(
 		}
 
 		const SelectLabel = () => <Label htmlFor={ forLabel || props.id }>{ label }</Label>;
-		const inlineLabel = isInline && label;
+		const inlineLabel = !! ( isInline && label );
 
 		return (
 			<>
 				{ label && ! isInline && <SelectLabel /> }
-				<div sx={ inlineLabel ? inlineStyles : {} } className="vip-select-component">
-					{ inlineLabel && <SelectLabel /> }
 
-					<div sx={ defaultStyles }>
-						<select
-							ref={ forwardRef }
-							sx={ {
-								width: '100%',
-								paddingLeft: 3,
-								paddingRight: 40, // 40px for the icon
-								py: 0,
-								borderColor: 'border',
-								borderRadius: 1,
-								appearance: 'none',
-								minHeight: '36px',
-								'&:focus': theme => theme.outline,
-								'&:focus-visible': theme => theme.outline,
-								'&:focus-within': theme => theme.outline,
-							} }
-							{ ...props }
-						>
-							{ placeholder && <option>{ placeholder }</option> }
-							{ options.map( ( { label: optionLabel, value, options: groupOptions } ) =>
-								value
-									? renderOption( optionLabel, value )
-									: renderGroup( optionLabel, groupOptions )
-							) }
-						</select>
+				<FormSelectContent isInline={ inlineLabel } label={ inlineLabel ? <SelectLabel /> : null }>
+					<select
+						ref={ forwardRef }
+						sx={ {
+							width: '100%',
+							paddingLeft: 3,
+							paddingRight: 40, // 40px for the icon
+							py: 0,
+							borderColor: 'border',
+							borderRadius: 1,
+							appearance: 'none',
+							minHeight: '36px',
+							'&:focus': theme => theme.outline,
+							'&:focus-visible': theme => theme.outline,
+							'&:focus-within': theme => theme.outline,
+						} }
+						{ ...props }
+					>
+						{ placeholder && <option>{ placeholder }</option> }
+						{ options.map( ( { label: optionLabel, value, options: groupOptions } ) =>
+							value ? renderOption( optionLabel, value ) : renderGroup( optionLabel, groupOptions )
+						) }
+					</select>
 
-						<MdExpandMore
-							aria-hidden="true"
-							size={ 24 }
-							sx={ {
-								paddingLeft: 2,
-								borderLeftWidth: '1px',
-								borderLeftStyle: 'solid',
-								borderLeftColor: 'border',
-								position: 'relative',
-								right: 34,
-								pointerEvents: 'none',
-							} }
-						/>
-					</div>
-				</div>
+					<FormSelectArrow />
+				</FormSelectContent>
 			</>
 		);
 	}
---
+++ src/system/NewForm/FormSelectInline.js
@@ -0,0 +1,31 @@
+export const inlineStyles = {
+	position: 'relative',
+	display: 'inline-flex',
+	alignItems: 'center',
+	borderColor: 'border',
+	borderRadius: 1,
+	borderWidth: 1,
+	borderStyle: 'solid',
+	paddingRight: 0,
+	paddingLeft: 3,
+
+	label: {
+		margin: 0,
+		paddingRight: 2,
+		borderRightWidth: '1px',
+		borderRightStyle: 'solid',
+		borderRightColor: 'border',
+	},
+
+	select: {
+		width: '100%',
+		border: 'none',
+		margin: 0,
+		paddingLeft: 2,
+	},
+
+	svg: {
+		right: 2,
+		position: 'absolute',
+	},
+};
