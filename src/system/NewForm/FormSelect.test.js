8e408dd992d7842b42d5bc0a01fa3067e2a663c3
Luiz Tiago Oliveira <luiztiago@gmail.com>
2022-09-12 13:28:53 -0300

    Add new specs to FormSelect

--- src/system/NewForm/FormSelect.test.js
+++ src/system/NewForm/FormSelect.test.js
@@ -21,9 +21,47 @@ const defaultProps = {
 	options,
 };
 
+const groupedProps = {
+	...defaultProps,
+	options: [
+		{
+			label: 'Group name',
+			options: [ options[ 0 ] ],
+		},
+		{
+			label: 'Another Group name',
+			options: [ options[ 1 ], options[ 2 ] ],
+		},
+	],
+};
+
 describe( '<FormSelect />', () => {
 	it( 'renders the FormSelect component', async () => {
 		const { container } = render( <FormSelect id="my_desert_list" { ...defaultProps } /> );
+
+		expect( screen.getByLabelText( defaultProps.label ) ).toBeInTheDocument();
+		expect( screen.getByRole( 'combobox' ) ).toBeInTheDocument();
+		expect( screen.getAllByRole( 'option' ) ).toHaveLength( 3 );
+		expect( screen.queryByRole( 'group' ) ).not.toBeInTheDocument();
+
+		// Check for accessibility issues
+		await expect( await axe( container ) ).toHaveNoViolations();
+	} );
+
+	it( 'renders the FormSelect component with optgroup when options are grouped', async () => {
+		const { container } = render( <FormSelect id="my_desert_list" { ...groupedProps } /> );
+
+		expect( screen.getByLabelText( defaultProps.label ) ).toBeInTheDocument();
+		expect( screen.getByRole( 'combobox' ) ).toBeInTheDocument();
+		expect( screen.getAllByRole( 'option' ) ).toHaveLength( 3 );
+		expect( screen.getAllByRole( 'group' ) ).toHaveLength( 2 );
+
+		// Check for accessibility issues
+		await expect( await axe( container ) ).toHaveNoViolations();
+	} );
+
+	it( 'renders the FormSelect component when isInline is true', async () => {
+		const { container } = render( <FormSelect id="my_desert_list" isInline { ...defaultProps } /> );
 
 		expect( screen.getByLabelText( defaultProps.label ) ).toBeInTheDocument();
 		expect( screen.getByRole( 'combobox' ) ).toBeInTheDocument();
