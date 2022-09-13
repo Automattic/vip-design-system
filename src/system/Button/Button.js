a922d02454f53d91c80c6cb651c5ed2793d60b2b
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-09-12 21:53:00 -0300

    Hover state for button links

--- src/system/Button/Button.js
+++ src/system/Button/Button.js
@@ -17,7 +17,10 @@ const Button = React.forwardRef( ( { sx, ...props }, forwardRef ) => (
 			justifyContent: 'center',
 			height: '36px',
 			py: 0,
-			textDecoration: props?.href ? 'underline' : 'none',
+			textDecoration: props?.to ? 'underline' : 'none',
+			'&:hover': {
+				textDecoration: 'none',
+			},
 			'&:focus': theme => theme.outline,
 			'&:focus-visible': theme => theme.outline,
 			'&:disabled': {
@@ -37,6 +40,7 @@ Button.displayName = 'Button';
 
 Button.propTypes = {
 	sx: PropTypes.object,
+	to: PropTypes.any,
 	className: PropTypes.any,
 };
 
