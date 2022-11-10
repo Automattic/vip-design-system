bad45fadda589cb032a2275b4ef051d9bf5b04ec
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-11-10 16:01:04 -0300

    Make Textarea based on Input to inherit all features

--- src/system/Form/Input.js
+++ src/system/Form/Input.js
@@ -10,45 +10,61 @@ import PropTypes from 'prop-types';
  * Internal dependencies
  */
 import { Validation, Label } from '../';
+import { Input as ThemeInput } from 'theme-ui';
+
+const RequiredLabel = () => (
+	<span sx={ { color: 'error', display: 'inline-block', ml: 2, fontSize: 1 } }>(Required)</span>
+);
+
+const inputStyles = {
+	unset: 'all',
+	border: '1px solid',
+	borderColor: 'border',
+	backgroundColor: 'card',
+	borderRadius: 1,
+	lineHeight: 'inherit',
+	px: 3,
+	py: 2,
+	fontSize: 2,
+	mb: 2,
+	color: 'text',
+	display: 'block',
+	width: '100%',
+	'&:focus': theme => theme.outline,
+	'&:focus-visible': theme => theme.outline,
+	'&:disabled': {
+		bg: 'backgroundSecondary',
+	},
+	'&::placeholder': {
+		color: 'placeholder',
+	},
+};
 
 const Input = React.forwardRef(
-	( { variant, label, forLabel, hasError, required, errorMessage, ...props }, ref ) => (
+	( { variant, label, forLabel, hasError, required, sx = {}, errorMessage, ...props }, ref ) => (
 		<React.Fragment>
 			{ label && (
 				<Label htmlFor={ forLabel }>
 					{ label }
-					{ required && '*' }
+					{ required && <RequiredLabel /> }
 				</Label>
 			) }
-			<input
-				{ ...props }
+
+			<ThemeInput
 				ref={ ref }
 				id={ forLabel }
 				required={ required }
-				sx={ {
-					border: '1px solid',
-					borderColor: 'border',
-					backgroundColor: 'card',
-					borderRadius: 1,
-					lineHeight: 'inherit',
-					px: 3,
-					py: 2,
-					fontSize: 2,
-					mb: 2,
-					color: 'text',
-					display: 'block',
-					width: '100%',
-					'&:focus': theme => theme.outline,
-					'&:focus-visible': theme => theme.outline,
-					'&:disabled': {
-						bg: 'backgroundSecondary',
-					},
-					'&::placeholder': {
-						color: 'placeholder',
-					},
-				} }
+				noValidate
+				aria-describedby={ hasError ? `describe-${ forLabel }-validation` : undefined }
+				sx={ { ...inputStyles, ...sx } }
+				{ ...props }
 			/>
-			{ hasError && errorMessage && <Validation>{ errorMessage }</Validation> }
+
+			{ hasError && errorMessage && (
+				<Validation isValid={ false } describedId={ forLabel }>
+					{ errorMessage }
+				</Validation>
+			) }
 		</React.Fragment>
 	)
 );
@@ -60,6 +76,7 @@ Input.propTypes = {
 	required: PropTypes.bool,
 	forLabel: PropTypes.string,
 	errorMessage: PropTypes.string,
+	sx: PropTypes.object,
 };
 
 Input.displayName = 'Input';
