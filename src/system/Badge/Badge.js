5efea299333e897d4d2843fd3fc9b84504d4ed07
Djalma Araújo de Andrade <djalma.araujo@automattic.com>
2022-12-20 16:20:22 -0300

    Badge: Adjust styling

--- src/system/Badge/Badge.js
+++ src/system/Badge/Badge.js
@@ -11,35 +11,46 @@ import PropTypes from 'prop-types';
  * Internal dependencies
  */
 import { Text } from '../';
+import { getVariants } from '../theme/getColor';
 
 const Badge = React.forwardRef(
-	( { variant = 'blue', sx, className = null, ...props }, forwardRef ) => (
-		<Text
-			as="span"
-			sx={ {
-				fontSize: 0,
-				padding: 0,
-				bg: `${ variant }.7`,
-				color: `${ variant }.85`,
-				py: 1,
-				verticalAlign: 'middle',
-				px: 2,
-				display: 'inline-block',
-				borderRadius: 1,
-				fontWeight: 'heading',
-				a: {
-					color: `${ variant }.65`,
+	( { variant = 'blue', sx, className = null, ...props }, forwardRef ) => {
+		const colorVariant = getVariants( `tag.${ variant }` );
+
+		return (
+			<Text
+				as="span"
+				sx={ {
+					fontSize: 0,
+					padding: 0,
+					bg: colorVariant.background,
+					color: colorVariant.text,
+					py: 1,
+					verticalAlign: 'middle',
+					px: 2,
+					display: 'inline-block',
+					borderRadius: 1,
+					fontWeight: 'heading',
 					'&:hover, &:focus': {
-						textDecoration: 'none',
+						backgroundColor: colorVariant.hover,
 					},
-				},
-				...sx,
-			} }
-			className={ classNames( 'vip-badge-component', className ) }
-			ref={ forwardRef }
-			{ ...props }
-		/>
-	)
+					a: {
+						color: colorVariant.text,
+						'&:hover, &:focus, &:active': {
+							textDecoration: 'none',
+						},
+						'&:active': {
+							color: colorVariant.active,
+						},
+					},
+					...sx,
+				} }
+				className={ classNames( 'vip-badge-component', className ) }
+				ref={ forwardRef }
+				{ ...props }
+			/>
+		);
+	}
 );
 
 Badge.displayName = 'Badge';
