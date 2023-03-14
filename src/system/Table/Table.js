9bc561230ee1e95e8146255f711d197717a1e9bb
Bruno Basto <b.basto@gmail.com>
2023-03-14 16:53:31 -0300

    Adds role and label to table container to follow a11y recommendations.
    https://www.a11yproject.com/posts/how-to-use-the-tabindex-attribute/#scrollable-overflow-containers

--- src/system/Table/Table.js
+++ src/system/Table/Table.js
@@ -3,11 +3,16 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+
+/**
+ * Internal dependencies
+ */
 import { screenReaderTextClass } from '../ScreenReaderText/ScreenReaderText';
 import { Box } from '../';
+import { generateId } from '../utils/random';
 
 const Table = React.forwardRef(
 	( { sx, className, children, caption = null, ...props }, forwardRef ) => {
@@ -16,10 +21,17 @@ const Table = React.forwardRef(
 			console.warn( '[A11Y] Please, add a caption to your table.' );
 		}
 
+		const captionId = useMemo( () => `table_caption_${ generateId() }`, [] );
+
 		return (
 			<Box
 				className={ classNames( 'vip-table-component', className ) }
 				sx={ { width: '100%', overflowX: 'auto' } }
+				role="region"
+				ariaLabelledby={ captionId }
+				// Because this container is scrollable, it needs to be focusable.
+				// A tabIndex value of 0 makes it focusable by keyboard and does not
+				// interfere with the tab order.
 				tabIndex={ 0 }
 			>
 				<table
@@ -28,7 +40,11 @@ const Table = React.forwardRef(
 					ref={ forwardRef }
 					{ ...props }
 				>
-					{ caption && <caption sx={ screenReaderTextClass }>{ caption }</caption> }
+					{ caption && (
+						<caption id={ captionId } sx={ screenReaderTextClass }>
+							{ caption }
+						</caption>
+					) }
 					{ children }
 				</table>
 			</Box>
