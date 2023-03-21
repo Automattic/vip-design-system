19602385e33bafb1a2727d3048be0e6eefdae434
Andrea Grassi <andrea.grassi@automattic.com>
2023-03-21 14:32:28 +0100

    added support for unplugin-icons in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,12 @@
 module.exports = {
 	stories: [ '../src/**/*.stories.jsx' ],
+	webpackFinal: async (config, { configType }) => {
+		config.plugins.push(
+			require('unplugin-icons/webpack')({ compiler: 'jsx', jsx: 'react' }));
+		// Make whatever fine-grained changes you need
+		// Return the altered config
+		return config;
+	},
 	addons: [
 		'@storybook/addon-a11y',
 		'@storybook/addon-essentials',
