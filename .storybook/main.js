4c8a110f033d633e926f067f536cf9170963e8ad
Andrea Grassi <andrea.grassi@automattic.com>
2023-03-21 14:32:14 +0100

    removed unused addon-docs that would raise error on build

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
 	stories: [ '../src/**/*.stories.jsx' ],
 	addons: [
 		'@storybook/addon-a11y',
-		'@storybook/addon-docs',
 		'@storybook/addon-essentials',
 		'@storybook/addon-links',
 		'@storybook/addon-controls',
