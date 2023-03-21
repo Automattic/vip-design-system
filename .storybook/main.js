0c81390bdd8d8b78b4c42212261112da8dcb72f9
Andrea Grassi <andrea.grassi@automattic.com>
2023-03-21 16:03:43 +0100

    removed unused customizer

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,13 +2,7 @@ module.exports = {
 	stories: [ '../src/**/*.stories.jsx' ],
 	webpackFinal: async (config, { configType }) => {
 		config.plugins.push(
-			require('unplugin-icons/webpack')({ compiler: 'jsx', jsx: 'react', iconCustomizer: (collection, icon, props) =>{
-					// customize this @iconify icon in this collection
-					if (props.size) {
-						props.width = props.size
-						props.height = props.size
-					}
-				}, }));
+			require('unplugin-icons/webpack')({ compiler: 'jsx', jsx: 'react' }));
 		// Make whatever fine-grained changes you need
 		// Return the altered config
 		return config;
