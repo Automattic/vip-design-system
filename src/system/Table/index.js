5686203b251a201c1fa7bb64deb0f9fea094d42c
chriszarate <chris.zarate@automattic.com>
2023-09-21 10:11:18 -0400

    Convert barrel file

--- src/system/Table/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Internal dependencies
- */
-import { Table } from './Table';
-import { TableRow } from './TableRow';
-import { TableCell } from './TableCell';
-
-export { Table, TableRow, TableCell };
