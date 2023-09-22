205107d2cd518792738b80d480f2fe6b66700b88
chriszarate <chris.zarate@automattic.com>
2023-09-22 15:36:16 -0400

    Remove unused Argument import

--- src/system/Heading/Heading.tsx
+++ src/system/Heading/Heading.tsx
@@ -3,7 +3,7 @@
  */
 import { forwardRef, Ref } from 'react';
 import { Heading as ThemeHeading, HeadingProps as ThemeHeadingProps } from 'theme-ui';
-import classNames, { Argument } from 'classnames';
+import classNames from 'classnames';
 
 export interface HeadingProps extends ThemeHeadingProps {
 	variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
