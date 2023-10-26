61f161a7f0b4f7d47e4754905f607e631509e273
Djalma Araujo <djalma.araujo@automattic.com>
2023-10-26 11:36:57 -0300

    Fix Radio positioning

--- src/system/Form/RadioBoxGroup.js
+++ src/system/Form/RadioBoxGroup.js
@@ -64,7 +64,7 @@ const RadioOption = ( {
 				id={ forLabel }
 				onChange={ onChangeHandler }
 				value={ `${ value }` }
-				sx={ { mr: 3, mt: 3 } }
+				sx={ { mr: 5, mt: 3 } }
 				aria-describedby={ describedById }
 				{ ...restOption }
 			/>
