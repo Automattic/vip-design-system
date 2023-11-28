9a0c011cecc3580f681e8423d47272a1ed32b10a
Djalma Araujo <djalma.araujo@automattic.com>
2023-11-28 17:17:06 -0300

    Simplify DialogCloseDefault

--- src/system/NewDialog/DialogClose.tsx
+++ src/system/NewDialog/DialogClose.tsx
@@ -9,10 +9,7 @@ interface DialogCloseProps {
 	children: React.ReactNode;
 }
 
-const DialogClose = forwardRef<
-	React.ElementRef< typeof DialogPrimitive.Close >,
-	DialogCloseProps
->( ( props, forwardedRef ) => (
+const DialogClose = forwardRef< HTMLButtonElement, DialogCloseProps >( ( props, forwardedRef ) => (
 	<DialogPrimitive.Close asChild { ...props } ref={ forwardedRef }>
 		{ props.children }
 	</DialogPrimitive.Close>
@@ -22,37 +19,36 @@ interface ThemeProps extends Theme {
 	outline?: Record< string, Record< string, string > >;
 }
 
-const DialogCloseDefault = forwardRef< React.ElementRef< typeof DialogClose > >(
-	( props, forwardedRef ) => (
-		<DialogClose { ...props } ref={ forwardedRef }>
-			<button
-				aria-label="Close"
-				sx={ {
-					all: 'unset',
-					fontFamily: 'inherit',
-					borderRadius: '100%',
-					height: 25,
-					width: 25,
-					display: 'inline-flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-					color: 'icon.primary',
-					position: 'absolute',
-					top: 4,
-					right: 4,
-					'&:hover': {
-						backgroundColor: 'borders.2',
-						outlineStyle: 'solid',
-						outlineColor: 'border.accent',
-						outlineWidth: '2px',
-					},
-					'&:focus, &:focus-visible': ( theme: ThemeProps ) => theme.outline,
-				} }
-			>
-				<IoClose aria-hidden="true" sx={ { fill: 'icon.primary' } } />
-			</button>
-		</DialogClose>
-	)
-);
+const DialogCloseDefault = forwardRef< HTMLButtonElement >( ( {}, forwardedRef ) => (
+	<DialogClose>
+		<button
+			ref={ forwardedRef }
+			aria-label="Close"
+			sx={ {
+				all: 'unset',
+				fontFamily: 'inherit',
+				borderRadius: '100%',
+				height: 25,
+				width: 25,
+				display: 'inline-flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				color: 'icon.primary',
+				position: 'absolute',
+				top: 4,
+				right: 4,
+				'&:hover': {
+					backgroundColor: 'borders.2',
+					outlineStyle: 'solid',
+					outlineColor: 'border.accent',
+					outlineWidth: '2px',
+				},
+				'&:focus, &:focus-visible': ( theme: ThemeProps ) => theme.outline,
+			} }
+		>
+			<IoClose aria-hidden="true" sx={ { fill: 'icon.primary' } } />
+		</button>
+	</DialogClose>
+) );
 
 export { DialogClose, DialogCloseDefault };
