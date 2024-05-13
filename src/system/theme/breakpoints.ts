b3b4edbec5d12db9c728026b3682c3a56542df83
Djalma Araujo <djalma.araujo@automattic.com>
2024-05-13 17:56:25 -0300

    Add 0 instead of 320px as the first breakpoint

--- src/system/theme/breakpoints.ts
+++ src/system/theme/breakpoints.ts
@@ -5,5 +5,11 @@ type Breakpoints = {
 export const generateBreakpoints = ( breakpoints: Breakpoints ) => {
 	const values = Object.values( breakpoints );
 
-	return values.map( bp => `${ bp }px` );
+	return values.map( ( bp, index ) => {
+		if ( index === 0 ) {
+			return `0px`;
+		}
+
+		return `${ bp }px`;
+	} );
 };
