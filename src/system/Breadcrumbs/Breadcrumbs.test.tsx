3e74db76094ce0c021a208b5984fb8eb99a8df16
Djalma Araujo <djalma.araujo@automattic.com>
2024-06-04 17:47:50 -0300

    Fix breadcrumb types test

--- src/system/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/system/Breadcrumbs/Breadcrumbs.test.tsx
@@ -1,8 +1,7 @@
 /** @jsxImportSource theme-ui */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
 import { ThemeUIProvider } from 'theme-ui';
 
