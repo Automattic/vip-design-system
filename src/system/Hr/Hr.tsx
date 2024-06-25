ec7a738b577731b0f4d872fe142cd47d0cd714e9
Djalma Araujo <djalma.araujo@automattic.com>
2024-06-25 16:28:59 -0300

    Add <Hr /> component to vip-design-system

---
+++ src/system/Hr/Hr.tsx
@@ -0,0 +1,9 @@
+import { ThemeUIStyleObject } from 'theme-ui';
+
+export type HrProps = {
+	sx?: ThemeUIStyleObject;
+};
+
+export const Hr = ( { sx, ...rest }: HrProps ) => (
+	<hr sx={ { my: 4, border: 0, height: '1px', backgroundColor: 'borders.2', ...sx } } { ...rest } />
+);
