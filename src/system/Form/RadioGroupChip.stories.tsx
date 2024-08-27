68b53d6129759b23136b4260c0b049f2199fd9b8
Ahmed Sayeed Wasif <ahmed.wasif@automattic.com>
2024-08-27 15:48:57 +0600

    Update doc

--- src/system/Form/RadioGroupChip.stories.tsx
+++ src/system/Form/RadioGroupChip.stories.tsx
@@ -16,16 +16,7 @@ export default {
 			description: {
 				component: `
 A radio-group-chip is a group of radio buttons that are styled as boxes. This component is used
-to allow users to select one option from a list of options.
-
-## Guidance
-
-### When to use the component
-
-- <strong>Select an option in a form.</strong> Use a radio-box-group when you want users to select
-a single option from a list of options.
-- <strong>Use as a toggle-group.</strong> Use a radio-box-group with the chip variant when you want
-to allow users to toggle between different options.
+to allow users to toggle between different options
 
 -------
 
