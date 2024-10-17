4d22f2e3e3e629ed37494986c624e49070e095c2
Katrina Tantay <katrina.tantay@automattic.com>
2024-10-16 19:53:40 -0500

    Fix tests

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,25 @@
-module.exports = {
-	ignore: [],
-	plugins: [],
-	presets: [
-		[
-			'@babel/preset-env',
-			{
-				loose: true,
-				modules: false,
-			},
+module.exports = function ( api ) {
+	const isTest = api.env( 'test' );
+	return {
+		ignore: [],
+		plugins: [],
+		presets: [
+			[
+				'@babel/preset-env',
+				{
+					loose: true,
+					modules: isTest ? 'auto' : false,
+				},
+			],
+			[
+				'@babel/preset-react',
+				{
+					importSource: 'theme-ui',
+					runtime: 'automatic',
+					throwIfNamespace: false,
+				},
+			],
+			'@babel/preset-typescript',
 		],
-		[
-			'@babel/preset-react',
-			{
-				importSource: 'theme-ui',
-				runtime: 'automatic',
-				throwIfNamespace: false,
-			},
-		],
-		'@babel/preset-typescript',
-	],
+	};
 };
