2ea5d7785375230eafa6756e332709bafa562dd6
Katrina Tantay <katrina.tantay@automattic.com>
2024-11-20 11:48:25 -0600

    Remove bg color to remove avatar border

--- src/system/Avatar/Avatar.tsx
+++ src/system/Avatar/Avatar.tsx
@@ -32,7 +32,6 @@ export const Avatar = forwardRef< HTMLElement, AvatarImageProps >(
 				width: size,
 				overflow: 'hidden',
 				border: 'none',
-				backgroundColor: 'icon.primary',
 				display: 'inline-flex',
 				alignItems: 'center',
 				justifyContent: 'center',
@@ -53,6 +52,7 @@ export const Avatar = forwardRef< HTMLElement, AvatarImageProps >(
 						borderRadius: '100%',
 						width: '100%',
 						display: 'block',
+						border: 'none',
 					} }
 				/>
 			) : (
