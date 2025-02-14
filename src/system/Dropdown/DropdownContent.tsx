5450ea07dcae4b2dc15247204de1c789e6e026f6
Rebecca Hum <rebeccahum@gmail.com>
2025-02-14 09:33:52 -0700

    Add align to DropdownContent and modify FilterDropdown to accept contentProps

--- src/system/Dropdown/DropdownContent.tsx
+++ src/system/Dropdown/DropdownContent.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 
 export interface DropdownContentProps {
 	className?: string;
+	align?: 'start' | 'center' | 'end';
 }
 
 export const styles = {
@@ -18,13 +19,14 @@ export const styles = {
 };
 
 export const DropdownContent = React.forwardRef< HTMLDivElement, DropdownContentProps >(
-	( { className, ...props }, forwardRef ) => (
+	( { className, align = 'center', ...props }, forwardRef ) => (
 		<DropdownMenuPrimitive.DropdownMenuContent
 			className={ classNames( 'vip-dropdown-menu-content', className ) }
 			ref={ forwardRef }
 			sx={ styles }
+			align={ align }
 			{ ...props }
-		/>
+		></DropdownMenuPrimitive.DropdownMenuContent>
 	)
 );
 
