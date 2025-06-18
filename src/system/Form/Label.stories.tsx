0e94b8c27e3c463dc7141ae6a7fdbfb82c8aa215
Joanne Lee <joanne.lee@automattic.com>
2025-06-17 17:47:27 -0700

    Update storybook for the Label component so we can test our changes in the UI locally

--- src/system/Form/Label.stories.tsx
+++ src/system/Form/Label.stories.tsx
@@ -1,36 +1,53 @@
 /** @jsxImportSource theme-ui */
-
-/**
- * Internal dependencies
- */
+import React from 'react';
 import { Form, Label } from '..';
 
 export default {
 	title: 'Form/Label',
+	component: Label,
+	parameters: { docs: { source: { type: 'auto' } } },
+	argTypes: {
+		children: {
+			control: 'text',
+			defaultValue: 'Label text',
+			description: 'The label content',
+		},
+		required: {
+			control: 'boolean',
+			defaultValue: false,
+			description: 'Whether to show the required asterisk',
+		},
+		clickable: {
+			control: 'boolean',
+			defaultValue: false,
+			description: 'Whether the label should have pointer cursor',
+		},
+		sx: {
+			control: 'object',
+			description: 'Theme UI sx-style overrides',
+		},
+		as: {
+			control: 'select',
+			options: [ 'label', 'span', 'div' ],
+			defaultValue: 'label',
+			description: 'The HTML element to render as',
+		},
+	},
 };
 
-const DefaultComponent = props => (
+const Template = args => (
 	<Form.Root>
-		<Label { ...props }>Label</Label>
+		<Label { ...args }>{ args.children }</Label>
 	</Form.Root>
 );
 
-export const Default = () => {
-	return (
-		<>
-			<DefaultComponent />
-		</>
-	);
+export const Default = Template.bind( {} );
+Default.args = {
+	children: 'Label text',
 };
 
-export const Required = () => {
-	const args = {
-		required: true,
-	};
-
-	return (
-		<>
-			<DefaultComponent { ...args } />
-		</>
-	);
+export const Required = Template.bind( {} );
+Required.args = {
+	children: 'Label text',
+	required: true,
 };
