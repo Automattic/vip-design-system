847d99ee64cc9f64cf1c21f559a15c742e86fbc8
Luiz Tiago Oliveira <luiztiago@gmail.com>
2025-07-03 15:31:14 -0300

    DescriptionList

---
+++ src/system/DescriptionList/DescriptionList.tsx
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import { forwardRef } from 'react';
+import { ThemeUIStyleObject } from 'theme-ui';
+
+/**
+ * Internal dependencies
+ */
+import { Grid } from '../Grid';
+
+interface DescriptionListProps {
+	list: {
+		label: string;
+		value: string;
+	}[];
+	className?: string;
+	sx?: ThemeUIStyleObject;
+	labelWidth?: string;
+}
+
+export const DescriptionList = forwardRef< HTMLDivElement, DescriptionListProps >(
+	( { sx, className, list, labelWidth = '100px' }: DescriptionListProps ) => (
+		<>
+			{ list.map( item => (
+				<Grid
+					className={ className }
+					as="dl"
+					key={ item.label }
+					columns={ [ `${ labelWidth } auto` ] }
+					sx={ {
+						fontSize: 2,
+						gap: 2,
+						alignItems: 'flex-start',
+						...sx,
+					} }
+				>
+					<dt>{ item.label }</dt>
+					<dd>{ item.value }</dd>
+				</Grid>
+			) ) }
+		</>
+	)
+);
+
+DescriptionList.displayName = 'DescriptionList';
