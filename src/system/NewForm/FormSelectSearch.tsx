470efcde5d2296acba0f7ba63e20902f5211affa
Luiz Tiago Oliveira <luiztiago@gmail.com>
2025-08-26 16:06:11 -0300

    Form Select Search fix

--- src/system/NewForm/FormSelectSearch.tsx
+++ src/system/NewForm/FormSelectSearch.tsx
@@ -7,22 +7,30 @@ import React from 'react';
 import { MdSearch } from 'react-icons/md';
 import { ThemeUIStyleObject } from 'theme-ui';
 
+/**
+ * Internal dependencies
+ */
+import { Box } from '../Box';
+
 interface FormSelectSearchProps {
 	sx?: ThemeUIStyleObject;
 }
 
-const searchStyles: ThemeUIStyleObject = {
+const wrapperStyles: ThemeUIStyleObject = {
 	position: 'absolute',
-	pr: 2,
 	left: 3,
+};
+
+const searchStyles: ThemeUIStyleObject = {
+	pr: 2,
 	pointerEvents: 'none',
 };
 
 export const FormSelectSearch = React.forwardRef< SVGSVGElement, FormSelectSearchProps >(
 	( props, forwardRef ) => (
-		<div ref={ forwardRef as React.RefObject< HTMLDivElement > }>
+		<Box ref={ forwardRef as React.RefObject< HTMLDivElement > } sx={ wrapperStyles }>
 			<MdSearch aria-hidden="true" size={ 24 } sx={ searchStyles } { ...props } />
-		</div>
+		</Box>
 	)
 );
 
